2568606caf1fd18ffc46db59abdba815095f828f
ThalaHemanth <viruhemanth@gmail.com>
2016-12-21 22:53:11 +0530

    initial commit

---
+++ app/components/Main.jsx
@@ -0,0 +1,21 @@
+var React = require('react');
+var NavBar = require('NavBar');
+
+var Main = React.createClass({
+    render: function() {
+        return (
+                <div>
+                    <div>
+                      
+                      <NavBar/>
+                      <h2> Main Component</h2>
+
+                      {this.props.children}
+                    </div>
+                  </div>
+
+              );
+    }
+});
+
+module.exports = Main;
---
+++ app/components/NavBar.jsx
@@ -0,0 +1,33 @@
+var React = require('react');
+var {Link,IndexLink} = require('react-router');
+
+var NavBar = React.createClass({
+
+    render: function () {
+        return (
+            <div>
+                <div className="top-bar">
+                    <div className="top-bar-left">
+                        <ul className="menu">
+                            <li className="menu-text">React Timer App
+                                <li> <IndexLink  activeClassName= "active" activeStyle = {{fontWeight: 'bold'}} to = '/'> Timer </IndexLink> </li>
+                                <li> <Link  activeClassName= "active" activeStyle = {{fontWeight: 'bold'}} to = '/'> Countdown </Link> </li>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="top-bar-right">
+                        <ul className="menu">
+                            <li className="menu-text">
+                                Created By <a href="https://github.com/ThalaHemanth" target="_blank"> Hemanth </a>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+
+        )
+    }
+});
+
+module.exports = NavBar;
